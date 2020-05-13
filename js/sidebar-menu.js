54480c4f65be0c4a3a75e41529939d3e36c20141
Denys Mazanov <dmazanov@mail.ru>
2020-05-13 21:26:25 +0200

    added script to open/close sidebar and animation for sidebar-menu

---
+++ js/sidebar-menu.js
@@ -0,0 +1,16 @@
+
+const openingBtn = document.querySelector('.sidebar__hamburger');
+const closingBtn = document.querySelector('.sidebar__close');
+const sidebar = document.querySelector('.sidebar');
+
+function openSidebar() {
+  sidebar.classList.add('sidebar--opened');
+}
+
+function closeSidebar() {
+  sidebar.classList.remove('sidebar--opened');
+}
+
+openingBtn.addEventListener("click", openSidebar);
+
+closingBtn.addEventListener("click", closeSidebar);
