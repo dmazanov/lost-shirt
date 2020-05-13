9dac93aa137d18ac7041b6eeb29536ad5ad8cd9a
Denys Mazanov <dmazanov@mail.ru>
2020-05-13 21:57:16 +0200

    added markup for fade-slide, added script for slider

--- js/sidebar-menu.js
+++ js/sidebar-menu.js
@@ -1,16 +1,17 @@
+(function() {
+  const openingBtn = document.querySelector('.sidebar__hamburger');
+  const closingBtn = document.querySelector('.sidebar__close');
+  const sidebar = document.querySelector('.sidebar');
 
-const openingBtn = document.querySelector('.sidebar__hamburger');
-const closingBtn = document.querySelector('.sidebar__close');
-const sidebar = document.querySelector('.sidebar');
+  function openSidebar() {
+    sidebar.classList.add('sidebar--opened');
+  }
 
-function openSidebar() {
-  sidebar.classList.add('sidebar--opened');
-}
+  function closeSidebar() {
+    sidebar.classList.remove('sidebar--opened');
+  }
 
-function closeSidebar() {
-  sidebar.classList.remove('sidebar--opened');
-}
+  openingBtn.addEventListener("click", openSidebar);
 
-openingBtn.addEventListener("click", openSidebar);
-
-closingBtn.addEventListener("click", closeSidebar);
+  closingBtn.addEventListener("click", closeSidebar);
+})();
